a90802af7b72b705851dbb06cccb20bc0916d49f
ZainabFara <zainabfarah050@gmail.com>
2024-11-25 20:32:34 +0100

    small change

--- src/components/Searchform/Searchform.jsx
+++ src/components/Searchform/Searchform.jsx
@@ -3,7 +3,7 @@ import React, { useRef, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../context';
-import './SearchForm.css';
+import './Searchform.css';
 
 const SearchForm = () => {
   const { setSearchTerm, setResultTitle } = useGlobalContext();
